d44219a6e9c90504bd0f00485dddee9e930b7363
Kim Nguyen <knguyenatl@gmail.com>
2016-02-22 20:02:59 -0500

    KN 022216

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function makeNewPosition(){
+var h = $(window).height() - 50;
+var w = $(window).width() - 50;
+var nh = Math.floor(Math.random() * h);
+var nw = Math.floor(Math.random() * w);
+return [nh,nw];
+}
+
+function animateDiv(){
+var newq = makeNewPosition();
+var oldq = $("#clay").offset();
+$("#clay").animate({top: newq[0], left: newq[1]}, 2000,"linear",function(){
+animateDiv();});
+}
+
+animateDiv();
